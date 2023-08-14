d545010bed8d46eda3735c5d3df4ba23061e3fcd
WellesleyMussolini <iei.muss@gmail.com>
2023-08-14 20:51:34 -0300

    feat: Introduce dynamic button visibility control function
    Integrate a versatile function designed to dynamically manage button visibility based on the countdown's running status. Leveraging an object-based approach, this function employs 'hidden' and 'visible' keys to toggle the display property of HTML buttons, ensuring an intuitive and responsive user interface.
    
    Wellesley Mussolini

--- code/utils/buttonsVisibility.js
+++ code/utils/buttonsVisibility.js
@@ -1,13 +1,20 @@
 import { toggleElementVisibility } from "./toggleElementVisibility.js";
 
-export const buttonVisibility = function (isRunningCountdown, start, pause, edit) {
+const start = document.querySelector('.start');
+const pause = document.querySelector('.pause');
+const reset = document.querySelector('.reset');
+const edit = document.querySelector(".edit");
+
+export const buttonVisibility = function (isRunningCountdown) {
     if (isRunningCountdown) {
         toggleElementVisibility.hidden(start);
         toggleElementVisibility.visible(pause);
+        toggleElementVisibility.hidden(reset);
         toggleElementVisibility.hidden(edit);
     } else {
         toggleElementVisibility.visible(start);
         toggleElementVisibility.hidden(pause);
+        toggleElementVisibility.visible(reset);
         toggleElementVisibility.visible(edit);
     };
 };
